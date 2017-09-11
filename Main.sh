5d85c26c08330b423b80cc8835c72053a94568a4
Gerardo-Pastor <31077829+Gerardo-Pastor@users.noreply.github.com>
2017-09-10 22:31:44 -0400

    Add files via upload

--- Main.sh
+++ Main.sh
@@ -33,20 +33,23 @@ while [ $fails != 0 ]; do         #salir o equivocarse 3 vece
             while [ 1 ];do                                  #Comandos a ingresar
                 printf "\033[01;34mPrebeShell@$usar: $PWD \033[01;33m" 
                 read  command
+                current=$PWD
+                cd $base
                 case "$command" in
-                    "ayuda")        ."$base"/commands.sh ayuda         ;;
-                    "cls")          clear                               ;;
-                    "infosis")      ."$base"/commands.sh infosys $pswd  ;;
-                    "arbol")        ."$base"/commands.sh arbol $base    ;;
-                    "fecha")        ."$base"/commands.sh fecha          ;;
-                    "hora")         ."$base"/commands.sh hora           ;;
-                    "busca")        ."$base"/commands.sh                ;;
-                    "pong")         ./pong.sh                           ;;
-                    "creditos")     ."$base"/commands.sh creditos       ;;
-                    "prebeplayer")  ."$base"/PrebePlayer.sh $pswd       ;;
-                    "salir")        exit 1                              ;;
-                    *)              $command                            ;;
+                    "ayuda")        ./commands.sh ayuda          ;;
+                    "infosis")      ./commands.sh infosys $pswd  ;;
+                    "arbol")        ./commands.sh arbol $base    ;;
+                    "fecha")        ./commands.sh fecha          ;;
+                    "hora")         ./commands.sh hora           ;;
+                    "busca")        ./commands.sh busca          ;;
+                    "pong")         ./pong.sh                    ;;
+                    "gato")         ./gato.sh                    ;;
+                    "creditos")     ./commands.sh creditos       ;;
+                    "prebeplayer")  ./PrebePlayer.sh $pswd       ;;
+                    "salir")        exit 1                       ;;
+                    *)              cd $current; $command        ;;
                 esac
+                cd $current
             done
         else
             echo -e "\n\033[31m Contrasena incorrecta te quedan $(($fails-1)) intentos \033[00m"
