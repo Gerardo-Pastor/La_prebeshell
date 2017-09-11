38ae7ff9cfac6c0b042362794436da5c3156d2f4
alejandro-08 <alejandro.montecillo@gmail.com>
2017-09-10 23:07:29 -0500

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-
+amarillo="\e[1;33m"
+rojo="\e[1;31m"
+azul="\e[1;34m"
+verde="\e[1;37m"
+r="\e[0m"
 case $1 in
 	"infosys") 
 		# Script que muestra la iformacion del sistema en que se esta manejando el bash.
-		az="\e[1;34m"
-		r="\e[0m"
+		
 		#Primero realizamos la busqueda en nuestro equipo real y los guardamos en variables. 
 		memTotal=`cat /proc/meminfo | grep "MemTotal" | grep "[1-9].*" -o`
 		memLibre=`cat /proc/meminfo | grep "MemFree" | grep "[1-9].*" -o`
@@ -17,45 +19,64 @@ case $1 in
 		echo -e "Memoria libre: $green "$memTotal" $r"
 		echo -e "Memoria total: $green "$memLibre" $r"
 		echo -e "Memoria swap total: $green "$memSwap" $r"
-		echo -e "El tipo de sistema operativo es: $az`cat /proc/sys/kernel/ostype`$r"
-		echo -e "La distribución del sistema es: $az`cat /proc/version_signature`$r"
-		echo -e "La versión del kernel es: $az $kernel $r"
+		echo -e "El tipo de sistema operativo es: $azul`cat /proc/sys/kernel/ostype`$r"
+		echo -e "La distribución del sistema es: $azul`cat /proc/version_signature`$r"
+		echo -e "La versión del kernel es: $azul $kernel $r"
 		echo -e "El modelo del procesador es:"
-		echo -e "$az $Procesador $r"
+		echo -e "$azul $Procesador $r"
 
 		for i in $(cat /proc/cpuinfo | grep "cpu MHz" | awk {'print $4$5'}); do
-			echo -e "Velocidad del procesador: $az $i [MHz]$r"
+			echo -e "Velocidad del procesador: $azul $i [MHz]$r"
 		done
 
 
 		for i in $(cat /proc/cpuinfo | grep "cache size" | awk {'print $4$5$6'}); do
-			echo -e "Memoria caché: $az $i $r"
+			echo -e "Memoria caché: $azul $i $r"
 		done
 	;;
 	"busca")
-		amarillo="\e[1;33m"
-		rojo="\e[1;31m"
-		azul="\E[34m\E[1m"
-		reset="\e[0m"
+		
 
-		echo -e "$azul Introduce el nombre de un archivo que quieras buscar : $reset"
-		read arch
-		echo -e "$azul Introduce la ruta en la que quieres buscar ese arhchivo: $reset" 
-		read rut
+		echo -e "$azul Introduce el nombre de un archivo que quieras buscar : $r"
+		read archivo
+		echo -e "$azul Introduce la ruta en la que quieres buscar ese arhchivo: $r" 
+		read ruta
 
-		if ls $rut | egrep -ow $arch
+		if ls $ruta | egrep -ow $archivo
 		then
-			echo -e "$amarillo Sí existe el archivo en la ruta especificada $reset"
+			echo -e "$amarillo Sí existe el archivo en la ruta especificada $r"
 		else
-			echo -e "$rojo No hay ningun archivo con ese nombre , no existe $reset"
+			echo -e "$rojo No hay ningun archivo con ese nombre , no existe $r"
 		fi
+     ;;
+	"hora" )
+	echo -n -e "$rojo La hora actual es: $r";
+	cat /proc/driver/rtc | egrep -o -m 1 [0-9]{2}+":"+[0-9]{2}+":"+[0-9]{2}	
 	;;
-	"creditos")
-		azul="\E[34m\E[1m"
-		rojo="\E[31m\E[1m"
-		amarillo="\E[33m\E[1m"
-		verde="\E[37m\E[32m\E[1m"
 
+	"fecha")
+	echo -n -e "$azul La fecha actual es: $r"
+	cat /proc/driver/rtc | egrep -o -m 1 [0-9]{4}+"-"+[0-9]{2}+"-"+[0-9]{2}
+
+	;;
+
+	"ayuda " )
+	
+	echo -e "$verde El comando ayuda te trae a este archivo :v $r"
+	echo -e "$azul El comando  busca te ayuda a encontrar un archivo en el sistema debes pasarle 2 parametros el nombre del archivo y la ruta  $r"
+	echo -e "$verde El comando hora te muestra la hora actual del sistema  $r"
+	echo -e "$azul El comando fecha   te muestra la fecha actual C: $r"
+ 	echo -e "$verde El comando infosys te da la informacion basica del equipo  $r"
+	echo -e "$azul El comando arbol te muestra un arbol de la ruta que le das  $r"
+	echo -e "$verde El comando creditos  te muestra la informacion de los creadores  $r"
+	echo -e "$azul El comando gato te dirige al juego del gato muy divertido :3 $r"
+	echo -e "$verde El comando pong te lleva al juego del pong xD  $r"
+	echo -e "$azul El comando prebeplayer te dirige a la prebeplayer donde podras reproducir musica :3 $r"
+	echo -e "$verde El comando salir te saca de aqui alv   $r"
+
+	;;
+
+	"creditos")
 		echo -e "\n\t\tLa Prebeshell "
 		echo -e "${rojo}Hecho por:"
 		echo -e "\t${azul}Jose Alejandro Montecillo Sandoval \n\tGerardo Alonso Pastor Olivera\n"
