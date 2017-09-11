ecaccf9045edec5f7ea9554afac4fe6fe35ea0e1
alejandro-08 <alejandro.montecillo@gmail.com>
2017-09-10 23:20:33 -0500

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -35,6 +35,8 @@ case $1 in
 		done
 	;;
 	"busca")
+		
+
 		echo -e "$azul Introduce el nombre de un archivo que quieras buscar : $r"
 		read archivo
 		echo -e "$azul Introduce la ruta en la que quieres buscar ese arhchivo: $r" 
@@ -46,58 +48,37 @@ case $1 in
 		else
 			echo -e "$rojo No hay ningun archivo con ese nombre , no existe $r"
 		fi
-     	;;
+     ;;
 	"hora" )
-		echo -n -e "$rojo La hora actual es: $r";
-		cat /proc/driver/rtc | egrep -o -m 1 [0-9]{2}+":"+[0-9]{2}+":"+[0-9]{2}	
+	echo -n -e "$rojo La hora actual es: $r";
+	cat /proc/driver/rtc | egrep -o -m 1 [0-9]{2}+":"+[0-9]{2}+":"+[0-9]{2}	
 	;;
+
 	"fecha")
-		echo -n -e "$azul La fecha actual es: $r"
-		cat /proc/driver/rtc | egrep -o -m 1 [0-9]{4}+"-"+[0-9]{2}+"-"+[0-9]{2}
+	echo -n -e "$azul La fecha actual es: $r"
+	cat /proc/driver/rtc | egrep -o -m 1 [0-9]{4}+"-"+[0-9]{2}+"-"+[0-9]{2}
+
 	;;
-	"ayuda " )
-	echo -e "$verde El comando ayuda te trae a este archivo :v $r"
-	echo -e "$azul El comando  busca te ayuda a encontrar un archivo en el sistema debes pasarle 2 parametros el nombre del archivo y la ruta  $r"
-	echo -e "$verde El comando hora te muestra la hora actual del sistema  $r"
-	echo -e "$azul El comando fecha   te muestra la fecha actual C: $r"
- 	echo -e "$verde El comando infosys te da la informacion basica del equipo  $r"
-	echo -e "$azul El comando arbol te muestra un arbol de la ruta que le das  $r"
-	echo -e "$verde El comando creditos  te muestra la informacion de los creadores  $r"
-	echo -e "$azul El comando gato te dirige al juego del gato muy divertido :3 $r"
-	echo -e "$verde El comando pong te lleva al juego del pong xD  $r"
-	echo -e "$azul El comando prebeplayer te dirige a la prebeplayer donde podras reproducir musica :3 $r"
-	echo -e "$verde El comando salir te saca de aqui alv   $r"
+
+	"ayuda" )
+	echo -e " --ESTE ES EL  MENÚ DE AYUDA CONSULTALO CADA VEZ QUE NECESITES AMIGO--$r"
+	echo -e "\t$verde El comando (ayuda) te trae a este archivo :v $r"
+	echo -e "\t$azul El comando  (busca) te ayuda a encontrar un archivo en el sistema debes pasarle 2 parametros el nombre del archivo y la ruta  $r"
+	echo -e "\t$verde El comando (hora) te muestra la hora actual del sistema  $r"
+	echo -e "\t$azul El comando (fecha )  te muestra la fecha actual C: $r"
+ 	echo -e "\t$verde El comando (infosys) te da la informacion basica del equipo  $r"
+	echo -e "\t$azul El comando (arbol) te muestra un arbol de la ruta que le das  $r"
+	echo -e "\t$verde El comando (creditos)  te muestra la informacion de los creadores  $r"
+	echo -e "\t$azul El comando (gato) te dirige al juego del gato muy divertido :3 $r"
+	echo -e "\t$verde El comando (pong) te lleva al juego del pong xD  $r"
+	echo -e "\t$azul El comando (prebeplayer) te dirige a la prebeplayer donde podras reproducir musica :3 $r"
+	echo -e "\t$verde El comando (salir) te saca de aqui alv de la Prebeshell    $r"
+
 	;;
+
 	"creditos")
 		echo -e "\n\t\tLa Prebeshell "
 		echo -e "${rojo}Hecho por:"
 		echo -e "\t${azul}Jose Alejandro Montecillo Sandoval \n\tGerardo Alonso Pastor Olivera\n"
 	;;
-	"arbol")
-		declare -i DIM=0;
-	arbolito(){
-	    cd "$1";
-	    for archivo in *
-	    do
-		printf "\e[1;35m>";
-		printf  "\e[0m";
-		for ((i=0; $i < DIM; i++))
-		do
-		    printf "\e[32m-";
-		    printf "\e[0m";
-		done
-		if [ -d "$archivo" ]; then
-		    printf "\e[1;36m";
-		else
-		    printf "\e[1;33m";
-		fi
-		printf "$archivo\e[0m\n";
-		if [ -d "$archivo" ]
-		then
-		    DIM=$DIM+1;
-		    arbolito "$archivo";
-		    cd ..;
-		fi
-	    done
-	 }
 esac
