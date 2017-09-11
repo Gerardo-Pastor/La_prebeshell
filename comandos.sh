81f7224a141b6f11fadfece89962a5cac90b0ec0
alejandro-08 <alejandro.montecillo@gmail.com>
2017-09-10 20:37:03 -0500

    Add files via upload

--- comandos.sh
+++ comandos.sh
@@ -31,8 +31,23 @@ for i in $(cat /proc/cpuinfo | grep "cache size" | awk {'print $4$5$6'}); do
 done
 
 
+#BUSCAR 
+amarillo="\e[1;33m"
+rojo="\e[1;31m"
+azul="\E[34m\E[1m"
+reset="\e[0m"
 
+echo -e "$azul Introduce el nombre de un archivo que quieras buscar : $reset"
+read arch
+echo -e "$azul Introduce la ruta en la que quieres buscar ese arhchivo: $reset" 
+read rut
 
+if ls $rut | egrep -ow $arch
+then
+	echo -e "$amarillo Sí existe el archivo en la ruta especificada $reset"
+else
+	echo -e "$rojo No hay ningun archivo con ese nombre , no existe $reset"
+fi
 
 
 #CREDITOS 
